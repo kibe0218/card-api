b506de30bb8b78261894d229304f3ceea7ee18ec
kibe0218 <kibe0218@gmail.com>
2025-12-10 22:38:03 +0900

    名称変更、updatecard追加（動作確認まだ）

---
+++ card/update_card.go
@@ -0,0 +1,52 @@
+package card
+
+import (
+	"card-api/firebase"
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+func UpdateCard(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	listID := r.URL.Query().Get("listId")
+	if listID == "" {
+		http.Error(w, "listIdを指定してね", http.StatusBadRequest)
+		return
+	}
+
+	userID := r.URL.Query().Get("userId")
+	if userID == "" {
+		http.Error(w, "userIdを指定してね", http.StatusBadRequest)
+		return
+	}
+
+	cardID := r.URL.Query().Get("cardId")
+	if cardID == "" {
+		http.Error(w, "cardIdを指定してね", http.StatusBadRequest)
+		return
+	}
+
+	var updated Card
+	if err := json.NewDecoder(r.Body).Decode(&updated); err != nil {
+		http.Error(w, "JSONの形式が正しくないっピ", http.StatusBadRequest)
+		return
+	}
+
+	_, err := firebase.FirestoreClient.Collection("users").
+		Doc(userID).
+		Collection("lists").
+		Doc(listID).
+		Collection("cards").
+		Doc(cardID).
+		Set(ctx, updated)
+	if err != nil {
+		http.Error(w, "Firestoreの更新に失敗したっピ", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "カード更新完了っピ",
+	})
+}
